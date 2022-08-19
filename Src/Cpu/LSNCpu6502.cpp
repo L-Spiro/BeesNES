9899258bbd24ac2dd208baa04ebfc525fd5a2d2d
L-Spiro <yogurtemperor@gmail.com>
2022-08-19 13:58:42 -0700

    Added references.

--- Src/Cpu/LSNCpu6502.cpp
+++ Src/Cpu/LSNCpu6502.cpp
@@ -4,6 +4,12 @@
  * Written by: Shawn (L. Spiro) Wilcoxen
  *
  * Description: Enough emulation of a Ricoh 6502 CPU to run a Nintendo Entertainment System.
+ * 
+ * https://www.nesdev.org/6502_cpu.txt
+ * http://www.oxyron.de/html/opcodes02.html
+ * http://www.6502.org/tutorials/6502opcodes.html#CLC
+ * http://users.telenet.be/kim1-6502/6502/proman.html
+ * http://problemkaputt.de/everynes.htm#cpu65xxmicroprocessor
  */
 
 
