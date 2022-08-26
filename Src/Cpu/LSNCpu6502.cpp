0c3c44c83da8ac899d1ae0cd37de754dc8b25f46
L-Spiro <yogurtemperor@gmail.com>
2022-08-25 21:36:04 -0700

    Adjusted comments.

--- Src/Cpu/LSNCpu6502.cpp
+++ Src/Cpu/LSNCpu6502.cpp
@@ -7,9 +7,10 @@
  * 
  * https://www.nesdev.org/6502_cpu.txt
  * http://www.oxyron.de/html/opcodes02.html
- * http://www.6502.org/tutorials/6502opcodes.html#CLC
+ * http://www.6502.org/tutorials/6502opcodes.html
  * http://users.telenet.be/kim1-6502/6502/proman.html
  * http://problemkaputt.de/everynes.htm#cpu65xxmicroprocessor
+ * https://www.masswerk.at/6502/6502_instruction_set.html
  */
 
 
--- Src/Cpu/LSNCpu6502.h
+++ Src/Cpu/LSNCpu6502.h
@@ -4,10 +4,13 @@
  * Written by: Shawn (L. Spiro) Wilcoxen
  *
  * Description: Enough emulation of a Ricoh 6502 CPU to run a Nintendo Entertainment System.
- *
- * Notes:
- *	The LSN_SF_BREAK flag is not stored in the CPU registers, it is only used when pushing to
- *	the stack inside PushStatusAndBAndSetAddressByIrq().
+ * 
+ * https://www.nesdev.org/6502_cpu.txt
+ * http://www.oxyron.de/html/opcodes02.html
+ * http://www.6502.org/tutorials/6502opcodes.html
+ * http://users.telenet.be/kim1-6502/6502/proman.html
+ * http://problemkaputt.de/everynes.htm#cpu65xxmicroprocessor
+ * https://www.masswerk.at/6502/6502_instruction_set.html
  */
 
 
